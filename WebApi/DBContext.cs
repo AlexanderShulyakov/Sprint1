4a331ce08b405916c9849a51351fff042d8ca3d2
alexander.shulyakov@hys-enterprise.com <75395113>
2023-09-29 23:53:09 +0300

    initial migration

--- WebApi/DBContext.cs
+++ WebApi/DBContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 public class WeatherContext : DbContext
 {
@@ -8,10 +9,28 @@ public class WeatherContext : DbContext
     {
 
     }
-}
+    protected override void ConfigureConventions(ModelConfigurationBuilder builder)
+    {
 
+        builder.Properties<DateOnly>()
+            .HaveConversion<DateOnlyConverter>()
+            .HaveColumnType("date");
+
+        base.ConfigureConventions(builder);
+
+    }
+}
+public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
+{
+    public DateOnlyConverter()
+        : base(dateOnly =>
+                dateOnly.ToDateTime(TimeOnly.MinValue),
+            dateTime => DateOnly.FromDateTime(dateTime))
+    { }
+}
 public class Record
 {
+    public int Id { get; set; }
     public DateOnly Date { get; set; }
     public int TemperatureC { get; set; }
     public int TemperatureF { get; set; }
